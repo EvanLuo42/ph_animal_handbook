61737bc6504e915e2a9ed31df6702ba777919675
Evan Luo <knacks.insects_0o@icloud.com>
2023-10-26 11:10:38 +0800

    feat(submission): Add accept submission action to admin site

--- submission/admin.py
+++ submission/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 
-# Register your models here.
+from animals.models import Animal
+from submission.models import Submission
+
+
+@admin.action(description='Accept a submission and move it to the animal list')
+def accept_submission(model_admin, request, queryset: QuerySet):
+    for submission in queryset:
+        Animal.objects.create(
+            name=submission.animal_name,
+            image=submission.animal_image,
+            category=submission.animal_category,
+            features=submission.animal_features
+        )
+        submission.delete()
+
+
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ['animal_name', 'email']
+    ordering = ['animal_name']
+    actions = [accept_submission]
+
+
+admin.site.register(Submission, SubmissionAdmin)
